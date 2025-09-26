75bc300d31663872c0dc42dce45620725cfd39f8
someshhh-45 <msomeshwar45@gmail.com>
2025-09-26 14:01:46 +0530

    Remove project_1 folder from repository

--- OneDrive/Desktop/streamlit/project_1/dashboard.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-file=st.file_uploader("upload your file :",type=["csv"])
-if file:
-    df=pd.read_csv(file)
-    st.subheader("data preview")
-    st.dataframe(df)
